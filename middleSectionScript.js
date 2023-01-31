c7cc45f1974ed6e287fef4129c3f101b796f8e0d
Hari Ram Karthik Manohar <hariram.manohar@solitontech.com>
2023-01-31 10:12:38 +0530

    Card created

--- middleSectionScript.js
+++ middleSectionScript.js
@@ -4,6 +4,11 @@ document.getElementById("rainy-icon").addEventListener("click", rainyIconClick);
 let sunnyDataList;
 let coldDataList;
 let rainyDataList;
+let city = document.querySelector("#card");
+let currentDataList;
+/**
+*function to execute when sunny icon is clicked
+*/
 function sunnyIconClick() {
   document
     .getElementById("sunny-icon")
@@ -22,7 +27,12 @@ function sunnyIconClick() {
   sunnyDataList.sort(
     (a, b) => b.temperature.split("°C")[0] - a.temperature.split("°C")[0]
   );
+  currentDataList = sunnyDataList;
+  createCard(sunnyDataList, "sunnyIcon");
 }
+/**
+ *function to execute when snow icon is clicked
+ */
 function snowIconClick() {
   document
     .getElementById("snow-icon")
@@ -42,7 +52,12 @@ function snowIconClick() {
   coldDataList.sort(
     (a, b) => b.precipitation.split("%")[0] - a.precipitation.split("%")[0]
   );
+  currentDataList = coldDataList;
+  createCard(coldDataList, "snowflakeIcon");
 }
+/**
+ *function to execute when rainy icon is clicked
+ */
 function rainyIconClick() {
   document
     .getElementById("rainy-icon")
@@ -60,4 +75,35 @@ function rainyIconClick() {
   rainyDataList.sort(
     (a, b) => b.humidity.split("%")[0] - a.humidity.split("%")[0]
   );
+  currentDataList = rainyDataList;
+  createCard(rainyDataList, "rainyIcon");
+}
+/**
+ *function to create cards
+ * @param {*} dataList
+ * @param {*} weatherCondition
+ */
+ function createCard(dataList, weatherCondition) {
+  let cardNumber = 0;
+  document.getElementById("all-cards").replaceChildren();
+  for (let i in dataList) {
+      let clone = city.cloneNode(true);
+      clone.id = "card" + cardNumber;
+      city.before(clone);
+      clone.querySelector("#city-name").innerText = dataList[i].cityName;
+      clone.querySelector("#city-temperature").innerText =
+        dataList[i].temperature;
+      clone.querySelector("#city-humidity").innerText = dataList[i].humidity;
+      clone.querySelector("#city-precipitation").innerText =
+        dataList[i].precipitation;
+      clone.querySelector("#city-temp-image").src =
+        "Assets/" + weatherCondition + ".svg";
+      clone.setAttribute(
+        "style",
+        "background-image:url('Assets/" +
+          dataList[i].cityName.toLowerCase() +
+          ".svg')"
+      );
+      document.getElementById("all-cards").appendChild(clone);
+  }
 }
