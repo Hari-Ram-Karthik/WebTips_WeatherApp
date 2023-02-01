8ff413955ec62786ab34c633a7be4a5537897178
Hari Ram Karthik Manohar <hariram.manohar@solitontech.com>
2023-02-01 09:33:40 +0530

    Sort updated

--- bottomSectionScript.js
+++ bottomSectionScript.js
@@ -1,7 +1,9 @@
-document.getElementById("sort-name").addEventListener("click", sortByName);
-document.getElementById("sort-temperature").addEventListener("click", sortByTemperature);
-let sortContinentName=document.getElementById("sort-name");
-let sortTemperature=document.getElementById("sort-temperature");
+document.getElementById("sort-name").addEventListener("click", sortByNameClick);
+document
+  .getElementById("sort-temperature")
+  .addEventListener("click", sortByTemperatureClick);
+let sortContinentName = document.getElementById("sort-name");
+let sortTemperature = document.getElementById("sort-temperature");
 let continent = document.querySelector("#continent");
 let allDataCopy = Object.values(allData);
 let timer;
@@ -10,6 +12,7 @@ let timer;
   setTimeCityName();
   clearInterval(timer);
   timer = setInterval(setTimeCityName, 500);
+  sortContinent();
 })();
 function createContinentCard() {
   let continentNumber = 0;
@@ -67,7 +70,33 @@ function getTime(timeZone) {
   }
   return time;
 }
-function sortByName() {
+function sortByNameClick() {
+  if (sortContinentName.name === "arrow-down") {
+    sortContinentName.src = "Assets/arrowUp.svg";
+    sortContinentName.name = "arrow-up";
+  } else {
+    sortContinentName.src = "Assets/arrowDown.svg";
+    sortContinentName.name = "arrow-down";
+  }
+  sortContinent();
+}
+function sortByTemperatureClick() {
+  if (sortTemperature.name === "arrow-down") {
+    sortTemperature.src = "Assets/arrowUp.svg";
+    sortTemperature.name = "arrow-up";
+  } else {
+    sortTemperature.src = "Assets/arrowDown.svg";
+    sortTemperature.name = "arrow-down";
+  }
+  sortContinent();
+}
+function sortContinent() {
+  let continentNameOrder;
+  let temperatureOrder;
+  continentNameOrder =
+    sortContinentName.name == "arrow-up" ? "Ascending" : "Descending";
+  temperatureOrder =
+    sortTemperature.name == "arrow-up" ? "Ascending" : "Descending";
   allDataCopy.sort(function (a, b) {
     if (a.timeZone.split("/")[0] < b.timeZone.split("/")[0]) {
       return -1;
@@ -77,27 +106,48 @@ function sortByName() {
     }
     return 0;
   });
-  if (sortContinentName.name === "arrow-down"){
-    sortContinentName.src = "Assets/arrowUp.svg";
-    sortContinentName.name="arrow-up";
-  }
-  else {
-    sortContinentName.src = "Assets/arrowDown.svg";
-    sortContinentName.name="arrow-down";
+  if (continentNameOrder == "Descending") {
     allDataCopy.reverse();
+  }
+  if (temperatureOrder == "Ascending") {
+    allDataCopy.sort(function (a, b) {
+      if (a.timeZone.split("/")[0] == b.timeZone.split("/")[0]) {
+        if (
+          Number(a.temperature.split("°C")[0]) <
+          Number(b.temperature.split("°C")[0])
+        ) {
+          return -1;
+        }
+        if (
+          Number(a.temperature.split("°C")[0]) >
+          Number(b.temperature.split("°C")[0])
+        ) {
+          return 1;
+        }
+        return 0;
+      }
+    });
+  } else {
+    allDataCopy.sort(function (a, b) {
+      if (a.timeZone.split("/")[0] == b.timeZone.split("/")[0]) {
+        if (
+          Number(a.temperature.split("°C")[0]) >
+          Number(b.temperature.split("°C")[0])
+        ) {
+          return -1;
+        }
+        if (
+          Number(a.temperature.split("°C")[0]) <
+          Number(b.temperature.split("°C")[0])
+        ) {
+          return 1;
+        }
+        return 0;
+      }
+    });
   }
   createContinentCard();
   setTimeCityName();
   clearInterval(timer);
   timer = setInterval(setTimeCityName, 500);
-}
-function sortByTemperature(){
-  if (sortTemperature.name === "arrow-down"){
-    sortTemperature.src = "Assets/arrowUp.svg";
-    sortTemperature.name="arrow-up";
-  }
-  else {
-    sortTemperature.src = "Assets/arrowDown.svg";
-    sortTemperature.name="arrow-down";
-  }
 }
